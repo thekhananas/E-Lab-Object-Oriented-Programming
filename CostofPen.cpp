f7122aea4a41cfc923aed07b085988974b8cd7f2
Anas Khan <chilloutwithanas@gmail.com>
2019-08-27 14:54:40 +0530

    Cost of Pen

---
+++ CostofPen.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int n;
+
+    void display()
+    {
+        cin >> n;
+    }
+};
+
+class B
+{
+public:
+    int c;
+
+    void display()
+    {
+        cin >> c;
+    }
+};
+
+class C : public A, public B
+{
+public:
+    void display()
+    {
+        A::display();
+        B::display();
+        cout << n * c;
+    }
+};
+
+int main()
+{
+    C sample;
+
+    sample.display();
+    return 0;
+}
