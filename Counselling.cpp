445bcf76fe164051f9731ebbd771d3c77fb5e926
Anas Khan <chilloutwithanas@gmail.com>
2019-08-28 21:30:21 +0530

    100%

--- Counselling.cpp
+++ Counselling.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Student
-{
-public:
-    char name1[100];
-    char name2[100];
-    float marks;
-
-    void getDetails()
-    {
-        cin >> name1 >> name2 >> marks;
+class Student {
+  public:
+  string name, mname;
+  float num;
+	void getDetails() {
+    	cin>>name>>mname>>num;
     }
-    void displayDetails()
-    {
-        cout << name1 << endl
-             << name2 << endl
-             << marks << endl;
+  
+  	void displayDetails() {
+    	cout<<name<<endl<<mname<<endl<<num;
     }
+  	
 };
 
-class StudentDetails : public Student
-{
+class StudentDetails:public Student {
+	
 };
-
-int main()
-{
-    StudentDetails sd;
-    sd.getDetails();
-    sd.displayDetails();
-    return 0;
+int main() {
+	StudentDetails sd;
+  	sd.getDetails();
+  	sd.displayDetails();
+	return 0;
 }
