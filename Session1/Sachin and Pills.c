0b0806e77a0511c74edb19086453ec9fb6d8ea19
Subham Choudhury <55877612+SubhamChoudhury@users.noreply.github.com>
2020-09-17 02:06:53 +0530

    Update Sachin and Pills.c

--- Session1/Sachin and Pills.c
+++ Session1/Sachin and Pills.c
@@ -1,43 +1,50 @@
-#include <stdio.h>
-int leap(int y)
-{
-    if (y % 400 == 0)
-        return 1;
-    else if (y % 4 == 0 && y % 100 != 0)
-        return 1;
-    else
-        return 0;
+ #include <iostream>
+using namespace std;
+class Month{
+  int days;
+  public:
+  Month(int,int);
+  bool leapYear(int);
+  int getDays();
+};
+Month::Month(int number,int year){
+  if(number<8) (number%2==1)?(days=31):(days=30);
+  else (number%2==0)?(days=31):(days=30);
+  if(number==2){
+    if(leapYear(year)==1) days=29;
+    else days=28;
+  }
 }
-int main()
-{
-    int t, m, y, d, a;
-    scanf("%d", &t);
-    for (int j = 1; j <= t; j++)
-    {
-        scanf("%d:%d:%d", &y, &m, &d);
-        if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-        {
-            a = ((31 - d) / 2) + 1;
-            printf("%d\n", a);
-        }
-        else if (m == 2)
-        {
-            if (leap(y))
-            {
-                a = ((29 - d) / 2) + 1;
-                printf("%d\n", a);
-            }
-            else
-            {
-                a = ((59 - d) / 2) + 1;
-                printf("%d\n", a);
-            }
-        }
-        else
-        {
-            a = ((61 - d) / 2) + 1;
-            printf("%d\n", a);
-        }
-    }
-    return 0;
+bool Month::leapYear(int year){
+  if(year%400==0) return 1;
+  if(year%100==0) return 0;
+  if(year%4==0) return 1;
+  return 0;
+}
+int Month::getDays(){
+  return days;
+}
+int pillDays(int day,int month,int year,int count=0){
+  Month M(month,year);
+  int monthDays = M.getDays(),i;
+  for(i=day;i<=monthDays;i++) if(i%2==1)count++;
+  if(monthDays%2==0){
+    pillDays(1,month+1,(month==12)?(year+1):(year),count);
+  }
+  else return count;
+}
+int main(){
+  int i,j,n,day,month,year,date[10];
+  char temp[10];
+  //char date[10];
+  cin>>n;
+  for(i=0;i<n;i++){
+    cin>>temp;
+    for(j=0;j<10;j++) date[j]=temp[j]-48;
+    day = date[8]*10 + date[9];
+    month = date[5]*10 + date[6];
+    year = date[0]*1000 + date[1]*100 + date[2]*10 + date[3];
+    cout << pillDays(day,month,year) << endl;
+  }
+  return 0;
 }
